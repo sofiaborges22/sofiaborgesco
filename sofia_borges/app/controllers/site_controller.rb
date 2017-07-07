5ca158fd7150c98fe9023683407cba1024fdb032
Sofia Borges <borgessofia22@gmail.com>
2017-07-07 19:44:39 -0400

    bootstrap and pages

--- sofia_borges/app/controllers/site_controller.rb
+++ sofia_borges/app/controllers/site_controller.rb
@@ -2,4 +2,16 @@ class SiteController < ApplicationController
 	def home
 		render 'home'
 	end
+
+	def portfolio
+		render 'portfolio'
+	end
+
+	def about
+		render 'about'
+	end
+
+	def contact
+		render 'contact'
+	end
 end
